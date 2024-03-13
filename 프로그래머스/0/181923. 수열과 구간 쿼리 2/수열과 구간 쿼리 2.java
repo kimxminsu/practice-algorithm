5c61a00a1836624904eb6c159a79f01ca1e35863
김민수 <35947676+kimxminsu@users.noreply.github.com>
2024-03-13 21:28:26 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 1.88 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = new int[queries.length];
+        java.util.Arrays.fill(answer, Integer.MAX_VALUE);
+        for (int i=0; i<queries.length; i++) {
+            int s = queries[i][0];
+            int e = queries[i][1];
+            int k = queries[i][2];
+            for (int j=s; j<=e; j++) {
+                if (arr[j] > k && arr[j] < answer[i]) {
+                    answer[i] = arr[j];
+                }
+            }
+            if (answer[i] == Integer.MAX_VALUE) {
+                answer[i] = -1;
+            }
+        }
+        return answer;
+    }
+}
