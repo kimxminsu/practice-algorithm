57d0b83c9a45df32430002b5a50a8937dc3b88c5
김민수 <35947676+kimxminsu@users.noreply.github.com>
2024-04-02 20:09:51 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 6.84 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.py
@@ -0,0 +1,10 @@
+def solution(arr, queries):
+    answer = arr
+    for i in range(len(queries)):
+        s = queries[i][0]
+        e = queries[i][1]
+        k = queries[i][2]
+        for j in range(s,e+1):
+            if j % k == 0:
+                answer[j] += 1
+    return answer
